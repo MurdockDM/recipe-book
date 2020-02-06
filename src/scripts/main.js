1ea4951618921b29d06d49a66d945e132cb490b6
Andy Collins <askingalot@gmail.com>
2020-02-06 09:42:10 -0600

    refactored add form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,9 @@
 import apiActions from "./api.js";
 import events from "./events.js"
 import renderRecipes from "./recipeListDom.js";
-import renderRecipeAddForm from "./recipeAddFormDom.js";
 import addRecipeAddEventListener from "./recipeAddEvents.js";
 
 // Show the form
-renderRecipeAddForm();
 addRecipeAddEventListener();
 
 events.recipeDeleteEventListener();
--- src/scripts/recipeAddFormDom.js
+++
@@ -1,9 +0,0 @@
-import createRecipeAddForm from "./recipeAddFormFactory.js";
-
-const recipeForm = document.querySelector("#recipeForm");
-
-const renderRecipeAddForm = () => {
-    recipeForm.innerHTML = createRecipeAddForm();
-}
-
-export default renderRecipeAddForm;
